99cd196d46484f4db4201e52278edf7b98b869dc
Pedro Américo Vinhas <pedrovinhas@protonmail.com>
2023-04-02 00:38:33 -0300

    Change struct Registers' name into Architecture and all occurences of 'registers' into 'arch'

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,13 @@ fn main() {
     let rom = init_rom(args[1].as_str());
     let mut ram: [u8; 0x1000] = [0; 0x1000];
     let v: [u8;16] = [0;16];
-    let mut registers = Registers::new();
-    registers.display = [1; WIDTH*HEIGHT];
+    let mut arch = Architecture::new();
+    arch.display = [1; WIDTH*HEIGHT];
     let stack = Stack::new();
     loop {
         // TODO: 60hz loop
-        dbg!(rom[registers.pc as usize], v, registers.display[1]);
-        execute(rom[registers.pc as usize], &mut registers, stack, v);
+        dbg!(rom[arch.pc as usize], v, arch.display[1]);
+        execute(rom[arch.pc as usize], &mut arch, stack, v);
     }
     
 }
@@ -28,27 +28,28 @@ fn init_rom(file_path: &str) -> Vec<u16> {
        .collect()
 }
 
-fn execute(instruction: u16, registers: &mut Registers, stack: Stack, v: [u8; 16]) -> () {
+fn execute(instruction: u16, arch: &mut Architecture, stack: Stack, v: [u8; 16]) -> () {
     match instruction {
-        0x00E0 => OpCodes::cls(&mut registers.display),
+        0x00E0 => OpCodes::cls(&mut arch.display),
         0x1000..=0x1FFF => {
-            OpCodes::jp(registers, instruction % 0x1000)
+            OpCodes::jp(arch, instruction % 0x1000)
         },
         0x2000..=0x2FFF => {
-            OpCodes::call(registers, stack, instruction % 0x1000)
+            OpCodes::call(arch, stack, instruction % 0x1000)
         },
         0x6000..=0x6FFF => {
             let x: u8 = ((instruction & 0x0F00) >> 8).try_into().unwrap(); // 2 * 4 bits
             let kk: u8 = (instruction & 0x00FF).try_into().unwrap();
-             OpCodes::load_byte(v, registers, x, kk);
+             OpCodes::load_byte(v, arch, x, kk);
         },
         0xDEAD => OpCodes::exit(),
         _ => panic!("OpCode does not exist!")
     }
-    registers.pc += 1;
+    arch.pc += 1;
 }
 #[derive(Debug, Clone, Copy)]
-pub struct Registers {
+pub struct Architecture {
+    
     v: [u8; 16],
     display: [u8; WIDTH*HEIGHT],
     i:  u16,
@@ -56,7 +57,7 @@ pub struct Registers {
     dt: u8,
     st: u8,
 }
-impl Registers {
+impl Architecture {
     fn new()->Self {
         Self { i: 0, pc: 0, dt: 0, st: 0, display: [0; WIDTH*HEIGHT], v: [0;16]}
     }
@@ -83,7 +84,7 @@ impl OpCodes {
          */ 
         *display = [0u8; 64*32];
     }
-    fn ret(registers: &mut Registers, stack:&mut Stack) -> () {
+    fn ret(registers: &mut Architecture, stack:&mut Stack) -> () {
         /*    00EE
          *   
          *    Return from a subroutine.
@@ -94,7 +95,7 @@ impl OpCodes {
         
         registers.pc = todo!();
     }
-    fn jp(registers: &mut Registers, nnn:u16) -> () {
+    fn jp(registers: &mut Architecture, nnn:u16) -> () {
         /*    1nnn
          *
          *    Jump to location nnn.
@@ -103,7 +104,7 @@ impl OpCodes {
          */
         registers.pc = nnn;
     }
-    fn call(registers: &mut Registers, mut stack:Stack, nnn:u16) -> () {
+    fn call(registers: &mut Architecture, mut stack:Stack, nnn:u16) -> () {
         /*    2nnn
          *
          *    Call subroutine at nnn.
@@ -116,7 +117,7 @@ impl OpCodes {
         stack.push(registers.pc);
         registers.pc = nnn;
     }
-    fn s_e_byte(v: [u8; 16], registers: &mut Registers, x: u8, kk:u8) -> () {
+    fn s_e_byte(v: [u8; 16], registers: &mut Architecture, x: u8, kk:u8) -> () {
         /*   3xkk
         *
         *    Skip next instruction if Vx == kk.
@@ -128,7 +129,7 @@ impl OpCodes {
             registers.pc += 2;
         }
     }
-    fn s_n_e_byte(v: [u8; 16], registers: &mut Registers, x: u8, kk:u8) -> () {
+    fn s_n_e_byte(v: [u8; 16], registers: &mut Architecture, x: u8, kk:u8) -> () {
         /*   4xkk
         *
         *    Skip next instruction if Vx != kk.
@@ -140,7 +141,7 @@ impl OpCodes {
             registers.pc += 2;
         }
     }
-    fn s_e_register(v: [u8; 16], registers: &mut Registers, x: u8, y:u8) -> () {
+    fn s_e_register(v: [u8; 16], registers: &mut Architecture, x: u8, y:u8) -> () {
         /*   5xy0
         *
         *    Skip next instruction if Vx == Vy.
@@ -152,7 +153,7 @@ impl OpCodes {
             registers.pc += 2;
         }
     }
-    fn load_byte(mut v: [u8; 16], registers: &mut Registers, x: u8, kk:u8) -> () {
+    fn load_byte(mut v: [u8; 16], registers: &mut Architecture, x: u8, kk:u8) -> () {
         /*   6xkk
          *   
          *   Set Vx = kk.
@@ -161,7 +162,7 @@ impl OpCodes {
          */
         v[x as usize] = kk;
     }
-    fn add_byte(mut v: [u8; 16], registers: &mut Registers, x: u8, kk:u8) -> () {
+    fn add_byte(mut v: [u8; 16], registers: &mut Architecture, x: u8, kk:u8) -> () {
         /*   7xkk
          *   
          *   Set Vx = Vx + kk.
