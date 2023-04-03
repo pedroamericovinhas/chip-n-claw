120bab0cf98689ea9d9b994eeb4da437d1501cc7
Pedro Américo Vinhas <pedrovinhas@protonmail.com>
2023-04-02 21:08:45 -0300

    rustfmt first pass

--- src/architecture/utils.rs
+++ src/architecture/utils.rs
@@ -4,5 +4,5 @@ impl Hex {
     pub fn swap_hex_digits(num: u16, i: u16, j: u16) -> u16 {
         let temp: u16 = (num >> (4 * i)) ^ (num >> (4 * j)) & 0xF;
         num ^ (temp << (4 * i)) ^ (temp << (4 * j))
-    } 
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let rom = init_rom(args[1].as_str());
     let mut arch = Architecture::new();
-    
+
     loop {
-        // TODO: 60hz loop                    
+        // TODO: 60hz loop
         arch.execute(rom.clone());
     }
 }
@@ -17,6 +17,6 @@ fn main() {
 fn init_rom(file_path: &str) -> Vec<u16> {
     let rom = fs::read(file_path).unwrap();
     rom.chunks_exact(2)
-       .map(|chunk| u16::from_le_bytes([chunk[1], chunk[0]]))
-       .collect()
+        .map(|chunk| u16::from_le_bytes([chunk[1], chunk[0]]))
+        .collect()
 }
